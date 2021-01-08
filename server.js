8bb5e0bb2bf4342f25cd617de440d3cff24ff862
(s) Samundra Gurung <samundra.gurung@students.plymouth.ac.uk>
2021-01-08 19:06:43 +0000

    cart page, login, and registration page finished

--- server.js
+++ server.js
@@ -8,16 +8,30 @@ const eLayout = require("express-ejs-layouts") //express layout
 
 const PORT = process.env.PORT || 8080
 
+
+
+// setting template.
+project.use(eLayout)
+project.set("views", path.join(__dirname, "/resources/views"))
+project.set("view engine", "ejs")
+
 //Assets for web
 project.use(express.static("public"))// defining where to get assets from.
 project.get("/", function(req, res){ //request and response.
     res.render("home")
 })
 
-// setting template.
-project.use(eLayout)
-project.set("views", path.join(__dirname, "/resources/views"))
-project.set("view engine", "ejs")
+project.get("/cart",(req, res)=>{
+    res.render("customers/cart")
+})
+
+project.get("/login",(req, res)=>{
+    res.render("auth/login")
+})
+
+project.get("/register",(req, res)=>{
+    res.render("auth/register")
+})
 
 project.listen(PORT, () =>{
     console.log(`listening on port ${PORT}`)
